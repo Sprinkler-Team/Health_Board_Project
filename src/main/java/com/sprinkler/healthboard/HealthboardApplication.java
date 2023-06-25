6091982a7fecc43b4655621e9d0aaaab8cfd0625
hotcoa <81072997+hotcoa922@users.noreply.github.com>
2023-06-25 22:31:01 +0900

    Feat: Initialize project

---
+++ src/main/java/com/sprinkler/healthboard/HealthboardApplication.java
@@ -0,0 +1,13 @@
+package com.sprinkler.healthboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprinkler/healthboard/HealthboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinkler.healthboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
