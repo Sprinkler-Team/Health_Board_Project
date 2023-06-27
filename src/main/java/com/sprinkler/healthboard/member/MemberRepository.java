47e65eb8adf2b0c20aab00555ac02c87619d569e
seungmio <68382824+seungmio@users.noreply.github.com>
2023-06-28 00:28:15 +0900

    Feat: Add repository interfaces for Member, Comment, and MemberRecommend classes

---
+++ src/main/java/com/sprinkler/healthboard/member/MemberRepository.java
@@ -0,0 +1,6 @@
+package com.sprinkler.healthboard.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
